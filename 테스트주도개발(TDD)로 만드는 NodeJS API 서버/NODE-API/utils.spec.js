67031d3c199ed94f487f4dacd01098debb20ba85
duduldy <52845846+duduldy@users.noreply.github.com>
2022-03-23 21:21:37 +0900

    코드, 스크립트 수정 추가

--- 테스트주도개발(TDD)로 만드는 NodeJS API 서버/NODE-API/utils.spec.js
+++ 테스트주도개발(TDD)로 만드는 NodeJS API 서버/NODE-API/utils.spec.js
@@ -1,16 +1,16 @@
 // assert 사용
+// const assert = require('assert');
 // const utils = require('./utils');
-// // const assert = require('assert');
 // describe('utils.js 모듈dml capitialize() 함수는', () => {
 //     it('문자열의 첫번째 문자를 대문자로 변환한다', () => {
 //         const result = utils.capitialize('hello');
-//         // assert.equal(result, 'Hello');
+//         assert.equal(result, 'Hello');
 //     })
 // });
 
 // should 사용
-const utils = require('./utils');
 const should = require('should');
+const utils = require('./utils');
 describe('utils.js 모듈dml capitialize() 함수는', () => {
     it('문자열의 첫번째 문자를 대문자로 변환한다', () => {
         const result = utils.capitialize('hello');
